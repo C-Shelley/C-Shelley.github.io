4ffd276915acbf56959d17c313ed33160f653d2e
Ciara <x00179789@mytudublin.ie>
2023-12-17 20:03:49 +0000

    website code

---
+++ IMD CA2/script.js
@@ -0,0 +1,9 @@
+let scrollSpy = new bootstrap.ScrollSpy(document.body, { target: '#navbar-main' })
+
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+  }
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
